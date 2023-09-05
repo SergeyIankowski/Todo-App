f7bfc69ec0d185f9685bbc248d7ec17936d5cd29
SergeyIankowski <garret933@gmail.com>
2023-09-05 15:47:51 +0300

    feat: create Todos component

---
+++ src/components/containers/Todos/index.tsx
@@ -0,0 +1,15 @@
+import { Divider, Flex } from "@chakra-ui/react";
+import { Todo } from "../view/Todo";
+
+export function Todos() {
+  return (
+    <Flex direction="column">
+      <Todo checked text="make first" />
+      <Divider />
+      <Todo checked text="make second" />
+      <Divider />
+      <Todo checked text="make third" />
+      <Divider />
+    </Flex>
+  );
+}
