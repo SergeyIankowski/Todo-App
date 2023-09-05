8f9e3145f83649355277a0c04ab53d881522ea58
SergeyIankowski <garret933@gmail.com>
2023-09-05 15:48:19 +0300

    feat: create AppLayout component

--- src/components/containers/AppLayout/index.tsx
+++ src/components/containers/AppLayout/index.tsx
@@ -1,5 +1,21 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { Todos } from "../Todos";
+import { TodoInput } from "../view/TodoInput";
 
 export function AppLayout() {
-  return <Flex>some text</Flex>;
+  return (
+    <Flex
+      width="100vw"
+      height="100vh"
+      direction="column"
+      alignItems="center"
+      bg="lightgray"
+    >
+      <Text fontSize="6xl">Todos</Text>
+      <Box width="1200px" bg="#ececec">
+        <TodoInput />
+        <Todos />
+      </Box>
+    </Flex>
+  );
 }
