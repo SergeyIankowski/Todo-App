691c0763f91701bada8f8f33fa9a471d07823f08
SergeyIankowski <garret933@gmail.com>
2023-09-06 12:56:16 +0300

    feat: implement adding new Todo to Store

--- src/components/view/TodoInput/index.tsx
+++ src/components/view/TodoInput/index.tsx
@@ -1,7 +1,39 @@
-import { Input } from "@chakra-ui/react";
+import { FormEvent, useState } from "react";
+import { Button, Flex, Input } from "@chakra-ui/react";
+import { v4 as uuidv4 } from "uuid";
+import { useTodosStore } from "@/store";
 
 export function TodoInput() {
+  const [todoText, setTodoText] = useState("");
+  const { setTodo } = useTodosStore();
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!todoText) return;
+
+    const newTodo = {
+      id: uuidv4(),
+      text: todoText,
+      complete: false,
+    };
+    setTodo(newTodo);
+    setTodoText("");
+  };
   return (
-    <Input placeholder="Enter your todo" size="lg" width="100%" bg="white" />
+    <form onSubmit={handleSubmit}>
+      <Flex gap="5px" alignItems="center">
+        <Input
+          placeholder="What needs to be done?"
+          size="lg"
+          width="100%"
+          bg="white"
+          value={todoText}
+          onChange={(e) => setTodoText(e.target.value)}
+        />
+        <Button type="submit" colorScheme="green">
+          Add New Todo
+        </Button>
+      </Flex>
+    </form>
   );
 }
