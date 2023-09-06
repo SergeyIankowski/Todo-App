9995b0d8a3a97b23249b70074b549e47a6d66fec
SergeyIankowski <garret933@gmail.com>
2023-09-06 12:58:07 +0300

    feat: add rendering Todos from store

--- src/components/containers/Todos/index.tsx
+++ src/components/containers/Todos/index.tsx
@@ -1,15 +1,39 @@
-import { Divider, Flex } from "@chakra-ui/react";
-import { Todo } from "../../view/Todo";
+import { useMemo } from "react";
+import { Box, Divider, Flex } from "@chakra-ui/react";
+import { Todo } from "@/components/view/Todo";
+import { useTodosStore } from "@/store";
+import { ITodo } from "@/interfaces/ITodo";
+import { DividerLabeled } from "@/components/view/DividerLabeled/Index";
 
 export function Todos() {
+  const { todos } = useTodosStore();
+  const completedTodos = useMemo(
+    () => todos.filter((item) => item.complete === true),
+    [todos]
+  );
+  const uncompletedTodos = useMemo(
+    () => todos.filter((item) => item.complete === false),
+    [todos]
+  );
+  const hasCompletedTodos = completedTodos.length !== 0;
+
   return (
     <Flex direction="column">
-      <Todo checked text="make first" />
-      <Divider />
-      <Todo checked text="make second" />
-      <Divider />
-      <Todo checked text="make third" />
-      <Divider />
+      {uncompletedTodos.map((todo: ITodo) => (
+        <Box key={todo.id}>
+          <Todo id={todo.id} text={todo.text} complete={todo.complete} />
+          <Divider />
+        </Box>
+      ))}
+      {hasCompletedTodos && (
+        <DividerLabeled content="Completed" color="#38A169" />
+      )}
+      {completedTodos.map((todo: ITodo) => (
+        <Box key={todo.id}>
+          <Todo id={todo.id} text={todo.text} complete={todo.complete} />
+          <Divider />
+        </Box>
+      ))}
     </Flex>
   );
 }
