3c867b9ccdf89a221f83422b342c823ab2af1129
SergeyIankowski <garret933@gmail.com>
2023-09-06 12:57:41 +0300

    feat: add changing todo status in Todo

--- src/components/view/Todo/index.tsx
+++ src/components/view/Todo/index.tsx
@@ -1,15 +1,22 @@
 import { Checkbox, Flex, Text } from "@chakra-ui/react";
+import { ITodo } from "@/interfaces/ITodo";
+import { useTodosStore } from "@/store";
 
-interface Props {
-  checked: boolean;
-  text: string;
-}
+type Props = ITodo;
 
-export function Todo({ text, checked }: Props) {
+export function Todo({ text, complete, id }: Props) {
+  const { changeTodoStatus } = useTodosStore();
+
   return (
-    <Flex justifyContent="space-between" width="100%" padding="0 20%">
-      <Checkbox isChecked={checked} size="lg" />
-      <Text fontSize="3xl" as="s">
+    <Flex
+      justifyContent="space-between"
+      width="100%"
+      padding="0 20%"
+      bg="white"
+      onClick={() => changeTodoStatus(id)}
+    >
+      <Checkbox size="lg" colorScheme="green" isChecked={complete} />
+      <Text fontSize="3xl" as={complete ? "s" : undefined}>
         {text}
       </Text>
     </Flex>
