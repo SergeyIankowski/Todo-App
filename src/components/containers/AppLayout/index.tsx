33390d858380e7e15f08f2669b3eb249872372f3
SergeyIankowski <garret933@gmail.com>
2023-09-06 12:59:21 +0300

    feat: add media queries and Buttons to AppLayout

--- src/components/containers/AppLayout/index.tsx
+++ src/components/containers/AppLayout/index.tsx
@@ -1,8 +1,16 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
-import { Todos } from "../Todos";
-import { TodoInput } from "../../view/TodoInput";
+import { Box, Flex, Stack, Text, useMediaQuery } from "@chakra-ui/react";
+import { Todos } from "@/components/containers/Todos";
+import { TodoInput } from "@/components/view/TodoInput";
+import { ClearCompletedButton } from "../ClearCompletedButton";
+import { RemoveAllTodosButton } from "../RemoveAllTodosButton";
+import { useTodosStore } from "@/store";
 
 export function AppLayout() {
+  const [isNarrowerThan1200] = useMediaQuery("(max-width: 1200px)");
+  const { todos } = useTodosStore();
+
+  const hasTodos = todos.length > 0;
+
   return (
     <Flex
       width="100vw"
@@ -12,9 +20,15 @@ export function AppLayout() {
       bg="lightgray"
     >
       <Text fontSize="6xl">Todos</Text>
-      <Box width="1200px" bg="#ececec">
+      <Box width={isNarrowerThan1200 ? "95%" : "1200px"} p="5px" bg="#ECECEC">
         <TodoInput />
         <Todos />
+        {hasTodos && (
+          <Stack direction="row" p={1}>
+            <ClearCompletedButton />
+            <RemoveAllTodosButton />
+          </Stack>
+        )}
       </Box>
     </Flex>
   );
